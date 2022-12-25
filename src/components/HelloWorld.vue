4a4f1b7fcf8ab126a570c053beae2b750398a45c
Marc <marc@dizid.com>
2022-12-25 11:01:57 +0100

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div>
    <div class="form-group">
      <label for="prompt">Je vraag aan chatGPT:</label>
-      <input type="text" class="form-control" v-model="prompt">  <!-- ref instead of v-model gives: TEST Prompt: "[object HTMLInputElement]"-->
+      <input type="text" class="form-control" v-model="prompt">
       <button class="btn btn-primary" @click="callAPI">Submit!</button> 
  </div>
      <div>
@@ -11,20 +11,14 @@
   <div>
  <p>TEST Prompt:    {{ prompt }}</p>
  <p>TEST api url: {{  apiurl  }}</p>
- <p>TEST tmp: {{  tmp  }}</p>
+ 
    </div></div>
  </template>
- <script setup>
+ <script setup>  // https://github.com/dizid/openAItest/blob/master/src/components/HelloWorld.vue
 import { ref} from "vue"
     const data = ref(null)
-    const prompt = ref('tell me about the sun')
-//    const tmp =  ref(prompt)  // WORKS, is still reactive
-//  const tmp = ref(prompt.value) // EROR: not reactive
-
-
-    const apiurl = "http://localhost:3000/openai?prompt=" + prompt.value
-
-    // this.$refs[ref].submit()  because your ref is a string and needs to be inside the [] to enable the dot notation.
+    const prompt = ref('tell me about the sun')  // App works with this default value. 
+    const apiurl = "http://localhost:3000/openai?prompt=" + prompt.value  // ERROR: not reactive anymore
 
     async function callAPI() {
       await new Promise((r) => setTimeout(r, 2000))
