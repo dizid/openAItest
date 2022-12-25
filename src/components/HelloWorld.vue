80d955bf2326ec7bcb0a2b51373b8cac61996d27
Marc <marc@dizid.com>
2022-12-25 10:54:59 +0100

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
    <div class="form-group">
-     <label for="prompt">Je vraag:</label>
-     <input type="text" class="form-control" v-model="prompt">
+     <label for="prompt">Je vraag aan chatGPT:</label>
+      <input type="text" class="form-control" v-model="prompt">  <!-- ref instead of v-model gives: TEST Prompt: "[object HTMLInputElement]"-->
       <button class="btn btn-primary" @click="callAPI">Submit!</button> 
  </div>
      <div>
@@ -10,16 +10,21 @@
    </div>
   <div>
  <p>TEST Prompt:    {{ prompt }}</p>
- <p>TEST api url {{  apiurl  }}</p>
-  </div></div>
+ <p>TEST api url: {{  apiurl  }}</p>
+ <p>TEST tmp: {{  tmp  }}</p>
+   </div></div>
  </template>
- <script async setup>
+ <script setup>
 import { ref} from "vue"
-
     const data = ref(null)
+    const prompt = ref('tell me about the sun')
+//    const tmp =  ref(prompt)  // WORKS, is still reactive
+//  const tmp = ref(prompt.value) // EROR: not reactive
 
-    const prompt = ref("the planets")
+
     const apiurl = "http://localhost:3000/openai?prompt=" + prompt.value
+
+    // this.$refs[ref].submit()  because your ref is a string and needs to be inside the [] to enable the dot notation.
 
     async function callAPI() {
       await new Promise((r) => setTimeout(r, 2000))
@@ -32,6 +37,5 @@ import { ref} from "vue"
     }
     return { data,}
     }
-
 </script>
 
