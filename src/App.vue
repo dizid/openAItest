5525adf8aa126556308890ac578b938552fb3693
Marc <marc@dizid.com>
2022-12-29 13:29:45 +0100

    v0.2

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
- <!--  <Suspense>
-    <HelloWorld/>
-  </Suspense> -->
-  <HelloWorld/>
+  <Suspense>
+    <HelloWorld />
+  </Suspense>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,43 @@
-
- <script setup>
+<script setup>
 import { ref, computed } from "vue";
 const data = ref(null);
-const query = ref("tell me about Bankok");
+const query = ref("vertel me over jezelf");
 const apiurl = computed(() => {
   return "http://localhost:3000/openai?q=" + query.value;
 });
 
 async function callAPI() {
-  // fetch API call to node.js server here
+  // create a fetch get API call to apiurl here and store the result in data
+  const response = await fetch(apiurl.value);
+  data.value = await response.json();
+  // log the second value of the json response to the console here  (data.value.message)
+  console.log("data.value.message: ", data.value.message);
 }
+// console.log("data.value.message: ", data.value.message);
+console.log("data.value: ", data.value);
+console.log("data: ", data);
 </script>
  <template>
-  <div class="form-group">
-    <label for="prompt">Je vraag aan chatGPT:</label>
-    <input type="text" class="form-control" v-model="query" />
-    <button class="btn btn-primary" @click="callAPI">Submit!</button>
-  </div>
-  <div>
-    <p>user typed: query: {{ query }}</p>
-    <p>url with the users query as url variable {{ apiurl }}</p>
+  <div class="container is-fluid">
+    <div class="columns">
+      <div class="column is-half">
+        <div class="box">
+          <label class="label" for="prompt">Je vraag aan chatGPT:</label>
+          <input class="input" type="text" v-model="query" /><br />
+          <button class="button is-info is-light" @click="callAPI">
+            Submit!
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-half">
+        <div class="box">
+          <label class="label" for="prompt">Antwoord van chatGPT:</label>
+          <!-- // show the second value of the json response here (data.value.message) -->
+          <p>{{ data ? data.message : "" }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
