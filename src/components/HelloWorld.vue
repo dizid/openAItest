a5f0a443c51a03051be17503c8ad4a6efefda952
Marc <marc@dizid.com>
2022-12-29 08:56:37 +0100

    4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,24 @@
-<template>
- <div>
-  <label>Type you query:</label>
-      <input type="text" class="form-control" v-model="query">
-      <p>user typed: query:  {{ query }}</p>
-       <p>url with the users query as url variable  {{ url }}</p> 
-   </div>  
-</template>
-<script setup>  
-import { ref} from "vue"
-const query = ref('defaultvalue')
-const url = "https://80fb-31-187-152-178.ngrok.io/openai?q=" + query.value
+
+ <script setup>
+import { ref, computed } from "vue";
+const data = ref(null);
+const query = ref("tell me about Bankok");
+const apiurl = computed(() => {
+  return "http://localhost:3000/openai?q=" + query.value;
+});
+
+async function callAPI() {
+  // fetch API call to node.js server here
+}
 </script>
+ <template>
+  <div class="form-group">
+    <label for="prompt">Je vraag aan chatGPT:</label>
+    <input type="text" class="form-control" v-model="query" />
+    <button class="btn btn-primary" @click="callAPI">Submit!</button>
+  </div>
+  <div>
+    <p>user typed: query: {{ query }}</p>
+    <p>url with the users query as url variable {{ apiurl }}</p>
+  </div>
+</template>
